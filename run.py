60a22d3a9a601d857db0b8222964bb3f952a780b
Ogah S. O <tedi242010@gmail.com>
2022-01-31 14:24:24 +0100

    Update run.py
    add

--- run.py
+++ run.py
@@ -1,12 +1,20 @@
 # import the necessary packages
 
 from app import create_app,db
+from flask_migrate import Migrate, upgrade
 from app.model import User, Task
 
 # instantiate the app run method
 app = create_app()
+migrate = Migrate(app, db)
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Task': Task}
+
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # migrate database to latest revision
+    upgrade()
