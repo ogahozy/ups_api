1e7ca29214fe18da4ecab0b55d7e5db1ff060a2b
Ogah S. O <tedi242010@gmail.com>
2022-02-03 20:51:27 +0100

    Update model.py
    me

--- app/model.py
+++ app/model.py
@@ -7,7 +7,7 @@ from datetime import datetime  # add
 from app import db,login #,bcrypt
 
 
-class User(UserMixin, db.Model):
+class Users(UserMixin, db.Model):
     __tablename__= 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
